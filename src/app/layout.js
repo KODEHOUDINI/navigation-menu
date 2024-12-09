07a9393f0e7454695ec312ba6490c6e39c516232
SauceCode <LESLEY.EDINAM@GMAIL.COM>
2024-12-09 03:03:27 +0000

    Fix: Font behaviour

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,5 @@
-import localFont from "next/font/local";
 import Menu from "@/components/menu/Menu";
 import "./globals.css";
-
-const shredHanded = localFont({
-  src: "./fonts/ShredHanded.woff",
-  variable: "--font-shred-handed",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "My Animated Navigation Menu",
@@ -16,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${shredHanded.variable}`}>
+      <body>
         <Menu />
         {children}
       </body>
